1ec0cf7feb0b820484d3aa499ed822d80a215d3a
Ishan-K <ishan.k.abhijeet@gmail.com>
2021-12-07 18:02:39 -0800

    Fixed Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ubr/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Ubr",
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: "Ubr"),
+    );
+  }
+}
+
